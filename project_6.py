1274472470d5185855d69516f896cf5b5073f1af
jfoppes <jffoppes@gmail.com>
2023-09-27 20:23:58 +0300

    Crete login system and account creation system

--- project_6.py
+++ project_6.py
@@ -3,3 +3,27 @@
 
 
 
+#game like pokenmon where you go down differnent paths and can run into pokemon on the way. 
+# Uses random to randomly slect a pokemon from a list for you to fight 
+# you fight the poken nad it cna wither be cought or run away attacks are effective or ineffective 
+# differnt color texts for diffenrt poeple or actions 
+#   ie prof oak one color 
+#   you heard a rumble in the busehd another 
+# for random path lengths: print ... in differnt ammouns based on list of []"......","..",".","..................."]
+#use turtle to visualize map
+#use tkinter to pupup new window when a fight is happeneing. 
+import turtle 
+import time
+import tkinter
+import random       
+'''Intro: Welocme to the game baisic tutorial.
+    Choose your name, color '''
+    
+def welcome():
+    pass
+
+
+'''Level 1: '''
+
+
+'''Level 2: '''
